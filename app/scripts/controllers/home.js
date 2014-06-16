698f6b24d75f6d7b79cf3f62abc176cfcca11615
Abhas Mittal <mittalabhas1@gmail.com>
2014-06-17 01:45:17 +0530

    feat(userDetails): saving userDetails to localStorage after login.

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -5,4 +5,12 @@ yearBook.controller('HomeCtrl', ['$scope', 'Restangular', '$state',
   	function ($scope, Restangular, $state) {
       if (!$scope.global.isUserAuthenticated)
         $state.transitionTo('login');
+
+      var restResource = Restangular.one('users', $scope.global.uid);
+      restResource.get().then(
+        function(response){
+          var userDetails = response.data[0];
+          console.log(userDetails);
+          localStorage.userDetails = JSON.stringify(userDetails);
+      });
   }]);
